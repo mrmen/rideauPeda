2d0c7bec615955c13b55e780520346f7455e38b5
Thomas Etcheverria <tetcheve@gmail.com>
2026-01-30 16:42:50 +0100

    Update rectangle.cpp

--- rectangle.cpp
+++ rectangle.cpp
@@ -2,71 +2,107 @@
 #include <windowsx.h>
 
 #define BORDER 30
-#define TOP_RESIZE 30
+
+RECT rect = {300, 200, 700, 500};
+POINT last;
+bool dragging = false;
+
+enum Mode { NONE, MOVE, RESIZE_TL, RESIZE_TR, RESIZE_BL, RESIZE_BR };
+Mode mode = NONE;
+
+bool InCornerTL(int x, int y){ return abs(x-rect.left)<BORDER && abs(y-rect.top)<BORDER; }
+bool InCornerTR(int x, int y){ return abs(x-rect.right)<BORDER && abs(y-rect.top)<BORDER; }
+bool InCornerBL(int x, int y){ return abs(x-rect.left)<BORDER && abs(y-rect.bottom)<BORDER; }
+bool InCornerBR(int x, int y){ return abs(x-rect.right)<BORDER && abs(y-rect.bottom)<BORDER; }
+bool InRect(int x, int y){ return x>rect.left && x<rect.right && y>rect.top && y<rect.bottom; }
 
 LRESULT CALLBACK WndProc(HWND hwnd, UINT msg, WPARAM wParam, LPARAM lParam)
 {
-    switch (msg)
+    switch(msg)
     {
-        case WM_NCHITTEST:
+        case WM_LBUTTONDOWN:
         {
-            POINT pt = {
-                GET_X_LPARAM(lParam),
-                GET_Y_LPARAM(lParam)
-            };
-            ScreenToClient(hwnd, &pt);
+            int x = GET_X_LPARAM(lParam), y = GET_Y_LPARAM(lParam);
+            last = {x,y};
 
-            RECT rc;
-            GetClientRect(hwnd, &rc);
+            if (InCornerTL(x,y)) mode = RESIZE_TL;
+            else if (InCornerTR(x,y)) mode = RESIZE_TR;
+            else if (InCornerBL(x,y)) mode = RESIZE_BL;
+            else if (InCornerBR(x,y)) mode = RESIZE_BR;
+            else if (InRect(x,y)) mode = MOVE;
+            else mode = NONE;
 
-            // Coins
-            if (pt.y < BORDER && pt.x < BORDER) return HTTOPLEFT;
-            if (pt.y < BORDER && pt.x > rc.right - BORDER) return HTTOPRIGHT;
-            if (pt.y > rc.bottom - BORDER && pt.x < BORDER) return HTBOTTOMLEFT;
-            if (pt.y > rc.bottom - BORDER && pt.x > rc.right - BORDER) return HTBOTTOMRIGHT;
-
-            // Bord haut (resize vertical)
-            if (pt.y < TOP_RESIZE) return HTTOP;
-
-            // Déplacement depuis n’importe où
-            return HTCAPTION;
+            if(mode!=NONE){ dragging=true; SetCapture(hwnd); }
+            break;
         }
 
-        case WM_KEYDOWN:
-            if (wParam == VK_ESCAPE)
-                PostQuitMessage(0);
+        case WM_MOUSEMOVE:
+            if(dragging)
+            {
+                int x = GET_X_LPARAM(lParam), y = GET_Y_LPARAM(lParam);
+                int dx = x-last.x, dy = y-last.y;
+                switch(mode)
+                {
+                    case MOVE: OffsetRect(&rect, dx, dy); break;
+                    case RESIZE_TL: rect.left+=dx; rect.top+=dy; break;
+                    case RESIZE_TR: rect.right+=dx; rect.top+=dy; break;
+                    case RESIZE_BL: rect.left+=dx; rect.bottom+=dy; break;
+                    case RESIZE_BR: rect.right+=dx; rect.bottom+=dy; break;
+                    default: break;
+                }
+                last={x,y};
+                InvalidateRect(hwnd,NULL,TRUE);
+            }
             break;
 
-        case WM_DESTROY:
-            PostQuitMessage(0);
+        case WM_LBUTTONUP:
+            dragging=false; mode=NONE;
+            ReleaseCapture();
             break;
+
+        case WM_KEYDOWN:
+            if(wParam==VK_ESCAPE) PostQuitMessage(0);
+            break;
+
+        case WM_PAINT:
+        {
+            PAINTSTRUCT ps;
+            HDC hdc=BeginPaint(hwnd,&ps);
+            FillRect(hdc,&ps.rcPaint,(HBRUSH)GetStockObject(BLACK_BRUSH));
+            EndPaint(hwnd,&ps);
+            break;
+        }
+
+        case WM_DESTROY: PostQuitMessage(0); break;
     }
-    return DefWindowProc(hwnd, msg, wParam, lParam);
+    return DefWindowProc(hwnd,msg,wParam,lParam);
 }
 
-int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE, LPSTR, int)
+int WINAPI WinMain(HINSTANCE hInst,HINSTANCE,LPSTR,int)
 {
-    WNDCLASS wc = {};
-    wc.lpfnWndProc = WndProc;
-    wc.hInstance = hInstance;
-    wc.lpszClassName = "RectanglePedagogique";
-    wc.hbrBackground = CreateSolidBrush(RGB(0,0,0));
+    WNDCLASS wc={};
+    wc.lpfnWndProc=WndProc;
+    wc.hInstance=hInst;
+    wc.lpszClassName="RectanglePedagogique";
+    wc.hbrBackground=CreateSolidBrush(RGB(0,0,0));
 
     RegisterClass(&wc);
 
-    HWND hwnd = CreateWindowEx(
-        0,
+    HWND hwnd=CreateWindowEx(
+        0,                  // Pas de WS_THICKFRAME
         wc.lpszClassName,
-        "Rectangle pédagogique",   // Titre visible dans la barre des tâches
-        WS_POPUP | WS_THICKFRAME,
-        300, 200, 400, 300,
-        nullptr, nullptr, hInstance, nullptr
+        "Rectangle pédagogique",
+        WS_POPUP,           // <- juste WS_POPUP pour fenêtre totalement noire
+        rect.left, rect.top,
+        rect.right-rect.left,
+        rect.bottom-rect.top,
+        nullptr,nullptr,hInst,nullptr
     );
 
-    ShowWindow(hwnd, SW_SHOW);
+    ShowWindow(hwnd,SW_SHOW);
 
     MSG msg;
-    while (GetMessage(&msg, nullptr, 0, 0))
+    while(GetMessage(&msg,nullptr,0,0))
     {
         TranslateMessage(&msg);
         DispatchMessage(&msg);
