447e3ecdfaf6203930ca94f37b39343f6b7ecc7e
Thomas Etcheverria <tetcheve@gmail.com>
2026-01-30 16:57:12 +0100

    Update rectangle.cpp

--- rectangle.cpp
+++ rectangle.cpp
@@ -8,57 +8,52 @@ LRESULT CALLBACK WndProc(HWND hwnd, UINT msg, WPARAM wParam, LPARAM lParam)
 {
     switch (msg)
     {
-        // 🔥 SUPPRESSION TOTALE DU NON-CLIENT AREA
         case WM_NCCALCSIZE:
-            if (wParam)
-                return 0;
+            if (wParam) return 0;
 
         case WM_NCHITTEST:
         {
-            POINT pt = {
-                GET_X_LPARAM(lParam),
-                GET_Y_LPARAM(lParam)
-            };
+            POINT pt = { GET_X_LPARAM(lParam), GET_Y_LPARAM(lParam) };
             ScreenToClient(hwnd, &pt);
 
             RECT rc;
             GetClientRect(hwnd, &rc);
 
-            // Coins
             if (pt.y < BORDER && pt.x < BORDER) return HTTOPLEFT;
             if (pt.y < BORDER && pt.x > rc.right - BORDER) return HTTOPRIGHT;
             if (pt.y > rc.bottom - BORDER && pt.x < BORDER) return HTBOTTOMLEFT;
             if (pt.y > rc.bottom - BORDER && pt.x > rc.right - BORDER) return HTBOTTOMRIGHT;
 
-            // Bord haut
             if (pt.y < TOP_RESIZE) return HTTOP;
-
-            // Déplacement
             return HTCAPTION;
         }
+
+        case WM_KEYDOWN:
+            if (wParam == VK_ESCAPE) PostQuitMessage(0);
+            break;
 
         case WM_DESTROY:
             PostQuitMessage(0);
             break;
     }
-    return DefWindowProc(hwnd, msg, wParam, lParam);
+    return DefWindowProcW(hwnd, msg, wParam, lParam);
 }
 
-int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE, LPSTR, int)
+int WINAPI wWinMain(HINSTANCE hInstance, HINSTANCE, LPWSTR, int)
 {
-    WNDCLASS wc = {};
+    WNDCLASSW wc = {};
     wc.lpfnWndProc = WndProc;
     wc.hInstance = hInstance;
-    wc.lpszClassName = "PureBlackRect";
+    wc.lpszClassName = L"RectanglePedagogique";
     wc.hbrBackground = CreateSolidBrush(RGB(0, 0, 0));
 
-    RegisterClass(&wc);
+    RegisterClassW(&wc);
 
-    HWND hwnd = CreateWindowEx(
+    HWND hwnd = CreateWindowExW(
         0,
         wc.lpszClassName,
-        "",
-        WS_POPUP | WS_THICKFRAME, // resize conservé
+        L"Rectangle pédagogique", // <- Wide string avec accents
+        WS_POPUP | WS_THICKFRAME,
         300, 200, 420, 300,
         nullptr, nullptr, hInstance, nullptr
     );
@@ -66,10 +61,10 @@ int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE, LPSTR, int)
     ShowWindow(hwnd, SW_SHOW);
 
     MSG msg;
-    while (GetMessage(&msg, nullptr, 0, 0))
+    while (GetMessageW(&msg, nullptr, 0, 0))
     {
         TranslateMessage(&msg);
-        DispatchMessage(&msg);
+        DispatchMessageW(&msg);
     }
     return 0;
 }
