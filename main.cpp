5f8da8f1b012c2bdbb98c398c27648550426f9e3
Thomas Etcheverria <tetcheve@gmail.com>
2026-01-30 16:07:36 +0100

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,20 +1,19 @@
 #include <windows.h>
-#include <windowsx.h>  // Pour GET_X_LPARAM / GET_Y_LPARAM
+#include <windowsx.h>
 
 #define BORDER 8
 #define TOP_RESIZE 10
-#define MIN_HEIGHT 120
 
-// Fonction WndProc
 LRESULT CALLBACK WndProc(HWND hwnd, UINT msg, WPARAM wParam, LPARAM lParam)
 {
     switch (msg)
     {
         case WM_NCHITTEST:
         {
-            POINT pt;
-            pt.x = GET_X_LPARAM(lParam);
-            pt.y = GET_Y_LPARAM(lParam);
+            POINT pt = {
+                GET_X_LPARAM(lParam),
+                GET_Y_LPARAM(lParam)
+            };
             ScreenToClient(hwnd, &pt);
 
             RECT rc;
@@ -26,51 +25,38 @@ LRESULT CALLBACK WndProc(HWND hwnd, UINT msg, WPARAM wParam, LPARAM lParam)
             if (pt.y > rc.bottom - BORDER && pt.x < BORDER) return HTBOTTOMLEFT;
             if (pt.y > rc.bottom - BORDER && pt.x > rc.right - BORDER) return HTBOTTOMRIGHT;
 
-            // Bord du haut
+            // Bord haut (resize vertical)
             if (pt.y < TOP_RESIZE) return HTTOP;
 
-            // Drag central
+            // Déplacement depuis le centre
             return HTCAPTION;
         }
-
-        case WM_COMMAND:
-            if (LOWORD(wParam) == 1)
-                PostQuitMessage(0);
-            break;
 
         case WM_DESTROY:
             PostQuitMessage(0);
             break;
     }
-
     return DefWindowProc(hwnd, msg, wParam, lParam);
 }
 
-// WinMain
 int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE, LPSTR, int)
 {
     WNDCLASS wc = {};
     wc.lpfnWndProc = WndProc;
     wc.hInstance = hInstance;
-    wc.lpszClassName = "BorderlessWindow";
+    wc.lpszClassName = "BlackRectangle";
     wc.hbrBackground = CreateSolidBrush(RGB(0,0,0));
+
     RegisterClass(&wc);
 
     HWND hwnd = CreateWindowEx(
         0,
         wc.lpszClassName,
-        "Rectangle Fenetre",
+        "",
         WS_POPUP | WS_THICKFRAME,
-        300, 200, 420, 300,
+        300, 200, 400, 300,
         nullptr, nullptr, hInstance, nullptr
     );
-
-    // ---- MENU ----
-    HMENU menu = CreateMenu();
-    HMENU fileMenu = CreatePopupMenu();
-    AppendMenu(fileMenu, MF_STRING, 1, "Quitter");
-    AppendMenu(menu, MF_POPUP, (UINT_PTR)fileMenu, "Fichier");
-    SetMenu(hwnd, menu);
 
     ShowWindow(hwnd, SW_SHOW);
 
@@ -80,6 +66,5 @@ int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE, LPSTR, int)
         TranslateMessage(&msg);
         DispatchMessage(&msg);
     }
-
     return 0;
 }
